f0ff49d2dc863657bb32bde1501545e28338a0f6
zabrat <57056861+zabrat@users.noreply.github.com>
2020-03-13 12:39:08 +0400

    Update botLawerTelegram.py
    No

--- botLawerTelegram.py
+++ botLawerTelegram.py
@@ -5,15 +5,15 @@ bot = telebot.TeleBot("965457992:AAGxFZYTn0SHf77Sis6mCoHZ1JzEZ2lIVqU")
 
 @bot.message_handler(commands=['start'])
 def greeting(message):
-	bot.send_message(message.chat.id, "Hello, my friend, how can I help you?")
+	bot.send_message(message.chat.id, "Здравствуйте, чем я могу Вам помочь?")
 @bot.message_handler(content_types=['text'])
 def lalala(message):
-	request = apiai.ApiAI('ca3d18eb99584581b4fdce3d63f02ba4').text_request()# Токен API к Dialogflow
-	request.lang = 'ru' # На каком языке будет послан запрос
-	request.session_id = 'BatlabAIBot'	# ID Сессии диалога (нужно, чтобы потом учить бота)
-	request.query = message.text # Посылаем запрос к ИИ с сообщением от юзера
+	request = apiai.ApiAI('ca3d18eb99584581b4fdce3d63f02ba4').text_request()
+	request.lang = 'ru'
+	request.session_id = 'BatlabAIBot'
+	request.query = message.text
 	responseJson = json.loads(request.getresponse().read().decode('utf-8'))
-	response = responseJson['result']['fulfillment']['speech'] # Разбираем JSON и вытаскиваем ответ
+	response = responseJson['result']['fulfillment']['speech']
 	if response:
 		bot.send_message(message.chat.id, text=response)
 	else:
